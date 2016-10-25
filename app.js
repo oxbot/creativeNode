8737ef3178d0877cb1b35479d3c8fd40f9cc684e
Jonathan Oxborrow <jon.oxbor@gmail.com>
2016-10-25 22:52:59 +0000

    added dropdowns and javascript function to grab values

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/getDetails', function(req, res) {
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log(' listening on port 3000!');
 });
 
