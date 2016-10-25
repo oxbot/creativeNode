8cb9a3df124f9f53fe0e286b6f42284b36185179
Jonathan Oxborrow <jon.oxbor@gmail.com>
2016-10-25 23:22:51 +0000

    got basic ajax working

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ res.sendFile('survey.html', {root: 'public'});
 
 app.get('/getDetails', function(req, res) {
 	console.log("in get details")
-	var jsonresult ="{{'trump': 300},{'hillary': 400},{'stein': 10},{'johnson':20},{'mcmulling':15}}"
+	var jsonresult ={'name': "trump"}
 			
 	res.status(200).json(jsonresult)
 });
