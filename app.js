70a8164bc29e967dbf904ed35eb2d75a1aad9239
Jonathan Oxborrow <jon.oxbor@gmail.com>
2016-10-25 22:11:53 +0000

    added getDetails route for returning survey data

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+
 
 app.get('/', function (req, res) {
 res.sendFile('survey.html', {root: 'public'});
 
 });
+
+app.get('/getDetails', function(req, res) {
+	console.log("in get details")
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
