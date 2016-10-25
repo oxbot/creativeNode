cc275d439ec13f0bdc17bd2e0f5fd249c31c2387
Jonathan Oxborrow <jon.oxbor@gmail.com>
2016-10-25 23:04:32 +0000

    getting the ajax connection working. Needs work still

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ res.sendFile('survey.html', {root: 'public'});
 
 app.get('/getDetails', function(req, res) {
 	console.log("in get details")
+	var jsonresult ="{{'trump': 300},{'hillary': 400},{'stein': 10},{'johnson':20},{'mcmulling':15}}"
+			
+	res.status(200).json(jsonresult)
 });
 
 
