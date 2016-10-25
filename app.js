06d498b4bb8b52998821bcf04ab6a4c36ac84b17
Jonathan Oxborrow <jon.oxbor@gmail.com>
2016-10-25 22:06:42 +0000

    added route and survey page

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+res.sendFile('survey.html', {root: 'public'});
+
 });
 
 app.listen(3000, function () {
